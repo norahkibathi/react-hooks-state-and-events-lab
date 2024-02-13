16fe47de2ae5a947ee0f14b5ffb9f3314383c645
norahkibathi <norah.njeri@student.moringaschool.com>
2024-02-13 03:57:19 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import itemData from "./Item"
+
 
 function App() {
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    
+    setIsDarkMode(!isDarkMode);}
+
   const appClass = false ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button  onClick={toggleDarkMode}>{isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
 import React from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  const filteredItems = selectedCategory === "All" ? items : items.filter(item => item.category === selectedCategory);
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={selectedCategory} onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
